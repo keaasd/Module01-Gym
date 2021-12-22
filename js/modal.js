0daa9018d3d76998b1a43160d86f08c39694ae0e
“Евгений <kea-asd@yandex.ru>
2021-12-23 01:49:24 +0700

    modal-popup

---
+++ js/modal.js
@@ -0,0 +1,18 @@
+
+function openModal() {
+    document.getElementById("modal").style.top = "0px";
+    document.getElementById("modal-popup").style.top = "1200px";
+}
+
+function closeModal() {
+    document.getElementById("modal").style.top = "-500px";
+    document.getElementById("modal-popup").style.top = "-700px";
+}
+
+// let popup = document.querySelector(".popup")
+// popup.addEventListener('click', openModal);
+// console.log(popup + "work");
+
+// let popupClose = document.querySelector(".popup-close")
+// popupClose.addEventListener('blur', closeModal);
+// console.log(popup + "nowork");
