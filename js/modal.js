e7a1c5f7b908334270f010c75bae0be309836877
“Евгений <kea-asd@yandex.ru>
2022-01-11 00:12:10 +0700

    mobile 320px OK

--- js/modal.js
+++ js/modal.js
@@ -1,23 +1,13 @@
 
 function openModal() {
     document.getElementById("modal").style.top = "0px";
-    document.getElementById("modal").style.display = "block";
     
     
     
     document.getElementById("modal-popup").style.top = "1200px";
-    document.getElementById("modal-popup").style.display = "block";
 }
 
 function closeModal() {
-    document.getElementById("modal").style.top = "-500px";
+    document.getElementById("modal").style.top = "-1000px";
     document.getElementById("modal-popup").style.top = "-700px";
 }
-
-// let popup = document.querySelector(".popup")
-// popup.addEventListener('click', openModal);
-// console.log(popup + "work");
-
-// let popupClose = document.querySelector(".popup-close")
-// popupClose.addEventListener('blur', closeModal);
-// console.log(popup + "nowork");
